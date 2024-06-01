d0924b02ef2474c025d95d84ab3723711852a554
Pranto Deb <rajprit0m@outlook.com>
2024-06-01 14:31:09 +0600

    banner section modified

--- src/components/HomeComponents/Banner.jsx
+++ src/components/HomeComponents/Banner.jsx
@@ -22,11 +22,16 @@ const Banner = () => {
                 className="mySwiper"
             >
                 <SwiperSlide>
-                    <div style={{ backgroundImage: `linear-gradient(to bottom, rgba(0,0,0,0.1), rgba(0,0,0,0.9)), url(${banner})` }} className="bg-cover bg-no-repeat py-40 px-60">
-                        <div className='text-White space-y-4'>
-                            <h1 className='text-7xl font-bold leading-tight'>An Amazing App <br /> That Does It All.</h1>
-                            <p>Voluptatem ipsa quae ab illo inventore veritatis et <br /> quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia.</p>
-                            <div>
+                    <div style={{ backgroundImage: `linear-gradient(to bottom, rgba(0,0,0,0.1), rgba(0,0,0,0.9)), url(${banner})` }} className="bg-cover bg-no-repeat py-12 md:py-20 lg:py-40 px-12 lg:px-60">
+                        <div className='text-White space-y-4 max-w-[1500px] mx-auto'>
+                            <h1 className='text-3xl md:text-7xl font-bold leading-tight'>An Amazing App <br /> That Does It All.</h1>
+                            <p className='text-xs md:text-lg'>Voluptatem ipsa quae ab illo inventore veritatis et <br /> quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia.</p>
+                            <div className='flex md:hidden'>
+                                <Button variant="contained" size="small" className='!bg-secondaryColor'>
+                                    Join as HR Manager
+                                </Button>
+                            </div>
+                            <div className='hidden md:flex'>
                                 <Button variant="contained" size="large" className='!bg-secondaryColor'>
                                     Join as HR Manager
                                 </Button>
@@ -35,11 +40,16 @@ const Banner = () => {
                     </div>
                 </SwiperSlide>
                 <SwiperSlide>
-                    <div style={{ backgroundImage: `linear-gradient(to bottom, rgba(0,0,0,0.1), rgba(0,0,0,0.9)), url(${banner2})` }} className="bg-cover bg-no-repeat py-40 px-60">
-                        <div className='text-White space-y-4'>
-                            <h1 className='text-7xl font-bold leading-tight'>An Amazing App <br /> That Does It All.</h1>
-                            <p>Voluptatem ipsa quae ab illo inventore veritatis et <br /> quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia.</p>
-                            <div>
+                    <div style={{ backgroundImage: `linear-gradient(to bottom, rgba(0,0,0,0.1), rgba(0,0,0,0.9)), url(${banner2})` }} className="bg-cover bg-no-repeat py-12 md:py-20 lg:py-40 px-12 lg:px-60">
+                        <div className='text-White space-y-4 max-w-[1500px] mx-auto'>
+                            <h1 className='text-3xl md:text-7xl font-bold leading-tight'>An Amazing App <br /> That Does It All.</h1>
+                            <p className='text-xs md:text-lg'>Voluptatem ipsa quae ab illo inventore veritatis et <br /> quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia.</p>
+                            <div className='flex md:hidden'>
+                                <Button variant="contained" size="small" className='!bg-secondaryColor'>
+                                    Join as HR Manager
+                                </Button>
+                            </div>
+                            <div className='hidden md:flex'>
                                 <Button variant="contained" size="large" className='!bg-secondaryColor'>
                                     Join as HR Manager
                                 </Button>
